4ab199eefe650dac472ff397bf1af270a061f5f5
tejasvarpe12 <140579868+tejasvarpe12@users.noreply.github.com>
2024-04-25 23:49:59 +0530

    updated accoring to test cases

--- xdisplayname/src/App.js
+++ xdisplayname/src/App.js
@@ -1,40 +1,57 @@
+import React, { useState } from 'react';
 import './App.css';
-import {useState} from "react";
 
 function App() {
-
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLasttName] = useState("");
-  const [fullName, setFullName] = useState("");
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [fullName, setFullName] = useState('');
+  const [firstNameError, setFirstNameError] = useState('');
+  const [lastNameError, setLastNameError] = useState('');
 
   const handleFirstNameChange = (event) => {
     setFirstName(event.target.value);
-  }
+    setFirstNameError('');
+  };
 
   const handleLastNameChange = (event) => {
-    setLasttName(event.target.value);
-  }
+    setLastName(event.target.value);
+    setLastNameError('');
+  };
 
   const handleSubmit = () => {
-    const fullNameValue = `${firstName} ${lastName}`;
-    setFullName(fullNameValue); 
-  }
+    if (!firstName.trim()) {
+      setFirstNameError('Please enter your first name');
+    }
+
+    if (!lastName.trim()) {
+      setLastNameError('Please enter your last name');
+    }
+
+    if (firstName.trim() && lastName.trim()) {
+      const fullNameValue = `${firstName} ${lastName}`;
+      setFullName(fullNameValue);
+    }
+  };
 
   return (
     <div className="container">
-      <div><h1>Full Name Display</h1></div>
-
-      <div className='inputGroup'>
-        <label htmlFor="firstName">First Name:</label>
-        <input id="firstName" type="text" value={firstName} onChange={handleFirstNameChange}/>
+      <div>
+        <h1>Full Name Display</h1>
       </div>
 
-      <div className='inputGroup'>
+      <div className={`inputGroup ${firstNameError && 'errorInput'}`}>
+        <label htmlFor="firstName">First Name:</label>
+        <input id="firstName" type="text" value={firstName} onChange={handleFirstNameChange} />
+        {firstNameError && <p className="error">{firstNameError}</p>}
+      </div>
+
+      <div className={`inputGroup ${lastNameError && 'errorInput'}`}>
         <label htmlFor="lastName">Last Name:</label>
         <input id="lastName" type="text" value={lastName} onChange={handleLastNameChange} />
+        {lastNameError && <p className="error">{lastNameError}</p>}
       </div>
 
-      <button onClick={handleSubmit}>
+      <button onClick={handleSubmit} type="submit">
         Submit
       </button>
 
